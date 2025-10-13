c4547a6c4fdb21021ca017d4ca02e8f7d27c1bb1
Vonky <deywidbraga14@gmail.com>
2025-10-13 19:21:15 -0300

    style: homeScreen (só o essencial)

--- AuxFit/app/(tabs)/home/index.tsx
+++ AuxFit/app/(tabs)/home/index.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { Colors, Spacing, Texts } from "@/constants/Styles";
+import { Colors, Spacing } from "@/constants/Styles";
 import {
   View,
   StyleSheet,
-  Alert,
-  Text,
   ScrollView,
   KeyboardAvoidingView,
   Platform,
@@ -12,56 +10,18 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
 import Background from "@/components/universal/Background";
-import Button from "@/components/universal/Button";
-import AddBtn from "@/components/universal/AddBtn";
-import CheckBtn from "@/components/universal/CheckBtn";
-import Favorite from "@/components/universal/FavoriteBtn";
-import FilterBtn from "@/components/universal/FilterBtn";
-import GenericModal from "@/components/universal/GenericModal";
-import Header from "@/components/universal/Header";
-import InputField from "@/components/universal/InputField";
-import WeeklyStreak from "@/components/universal/WeeklyStreak";
-import WeightIn from "@/components/universal/WeightIn";
-import ProgressBar from "@/components/questions/ProgressBar";
-import FilterModal from "@/components/universal/FilterModal";
-import MacrosTable from "@/components/diet/MacrosTable";
-import MacroDonutChart from "@/components/diet/MacroDonutChart";
-import MacroTableLegend from "@/components/diet/MacroTableLegend";
 import MacrosProgress from "@/components/diet/MacrosProgress";
 import WaterProgress from "@/components/diet/WaterProgress";
+import Button from "@/components/universal/Button";
 
 export default function HomeScreen() {
   const router = useRouter();
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isFilterVisible, setIsFilterVisible] = useState(false);
-  const [nameValue, setNameValue] = useState("");
-  const [passwordValue, setPasswordValue] = useState("");
-
-  const handleStartWorkout = () => {
-    Alert.alert("Treino", "Iniciando treino!");
-  };
-
-  const handleAddNew = () => {
-    console.log("Adicionar novo item");
-  };
-
-  const handleFilter = () => {
-    setIsFilterVisible((prev) => !prev);
-  };
-
-  const handleGenericModal = () => {
-    setIsModalVisible((prev) => !prev);
-  };
-
-  const handleHeaderIcon = () => {
-    Alert.alert("Opções", "Menu de opções");
-  };
+  const [isModalVisible] = useState(false);
 
   const handleTestOnboarding = () => {
     router.push("/onboarding");
   };
 
-  // Dados de mock para teste
   const macros: { protein: number; carbs: number; fats: number } = {
     protein: 150,
     carbs: 225,
@@ -92,82 +52,12 @@ export default function HomeScreen() {
           showsVerticalScrollIndicator={false}
         >
           <View style={styles.scrollContent}>
-            {/* Cabeçalho */}
-            {/* <Header
-              title="Home"
-              subtitle="Bem-vindo ao seu treino"
-              icon="more-vert"
-              onIconPress={handleHeaderIcon}
-            />
 
             <Button
               title="Testar Onboarding"
               onPress={handleTestOnboarding}
               bgColor="#35e1ffff"
             />
-
-            <View>
-              <ProgressBar />
-            </View>
-
-            <WeeklyStreak />
-
-            <WeightIn />
-
-            <InputField
-              icon="person"
-              placeholder="Digite seu nome"
-              value={nameValue}
-              onChangeText={setNameValue}
-            />
-
-            <InputField
-              icon="lock"
-              placeholder="Digite sua senha"
-              password={true}
-              value={passwordValue}
-              onChangeText={setPasswordValue}
-            />
-
-            <InputField placeholder="Email" keyboardType="email-address" />
-
-            <Button title="Iniciar Treino" onPress={handleStartWorkout} />
-
-            <View style={styles.smallButtonsRow}>
-              <AddBtn onPress={handleAddNew} />
-              <CheckBtn />
-              <Favorite />
-              <FilterBtn onPress={handleFilter} />
-
-              <FilterModal
-                isFilterVisible={isFilterVisible}
-                filterTitle="Teste"
-                onClose={handleFilter}
-              >
-                <Text>Olá</Text>
-              </FilterModal>
-            </View>
-
-            <Button title="Abrir Modal Genérico" onPress={handleGenericModal} /> */}
-
-            <MacrosTable
-              calories={calories}
-              protein={macros.protein}
-              carbs={macros.carbs}
-              fats={macros.fats}
-            />
-            <View style={styles.macrosContainer}>
-              <MacroTableLegend
-                protein={macros.protein}
-                carbs={macros.carbs}
-                fats={macros.fats}
-              />
-              <MacroDonutChart
-                protein={macros.protein}
-                carbs={macros.carbs}
-                fats={macros.fats}
-              />
-            </View>
 
             <MacrosProgress
               calories={calories}
@@ -179,17 +69,12 @@ export default function HomeScreen() {
             />
 
             <WaterProgress currentWater={0}/>
+
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
 
-      {/* Modal genérico */}
-      <GenericModal isVisible={isModalVisible} onClose={handleGenericModal}>
-        <Text style={{ color: "#fff", fontSize: 18, marginBottom: 16 }}>
-          Modal Genérico
-        </Text>
-        <Text style={{ color: "#fff" }}>Este é um modal personalizável.</Text>
-      </GenericModal>
+
     </SafeAreaView>
   );
 }
